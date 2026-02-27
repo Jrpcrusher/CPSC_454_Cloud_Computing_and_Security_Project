35d0ad73b07b3d6244b14e1c84ec7ae7ae067649
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-02-26 19:28:50 -0800

    Created routes for admin.py

---
+++ scan-and-save/backend/app/api/routes/admin.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/users") # Method to get a list of all users in the system
+def users():
+    return []
+
+@router.delete("/users/{user_id}") # Method to delete users
+def delete_user(user_id: str):
+    # TODO: Make this method delete the user account specified by the ID, along with remove all related artwork
+    return {"deleted": user_id}
+
+@router.put("/users/{user_id}/role") # Method to escalate privileges of a user account to admin
+def admin(user_id: str):
+    # TODO: Make this method take the user id, and give that user admin permissions to be a site admin
+    return {"admin": user_id}
+
+@router.delete("/items/{item_id}")
+def delete_item(item_id: str):
+    # TODO: Make this method take the item id, and give the admin the ability to remove this item from the website
+    return {"deleted": item_id}
