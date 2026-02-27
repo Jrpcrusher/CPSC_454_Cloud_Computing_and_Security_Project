0f5175b019d348fc6310f3457d4ce2f3770a10ea
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-02-26 19:22:09 -0800

    auth.py myaccount route to me

--- scan-and-save/backend/app/api/routes/auth.py
+++ scan-and-save/backend/app/api/routes/auth.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/register") # Handles new user account creation
+@router.post("/register") # Handles new user account creation
 def register():
     # TODO: On account creation, make new account identifier (ID), and establish password and email associated
-    return {"Status:", "account_created"}
+    return {"status": "account_created"}
 
-@router.get("/login") # Handles logging in the user
+@router.post("/login") # Handles logging in the user
 def login():
     status = 0; # Default status of 0 for user not logged in
     # TODO: get_credentials(), email/username and password
@@ -16,16 +16,16 @@ def login():
         return {"status": "login_failed"}
     return {"status": "login_success"}
 
-@router.get("/logout") # Handles the user logging out of their account
+@router.post("/logout") # Handles the user logging out of their account
 def logout():
     return {"status": "logout_success"}
 
-@router.get("/myaccount") # Handles viewing the users own profile and account
-def myaccount():
+@router.get("/me") # Handles viewing the users own profile and account
+def me():
     account = None # TODO: Make method to return account through this function
     return {"account": account}
 
-@router.get("/admin") # Handles admin login raising privileges
+@router.post("/admin") # Handles admin login raising privileges
 def admin():
     admin = 0; # Return 1 if user is admin, 0 if not
     # Also enable MFA for this part, admins always will need MFA enabled via auth app
