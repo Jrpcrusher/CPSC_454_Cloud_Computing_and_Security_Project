56f3701298e997927ee06b003b640f1635e8388a
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-03-13 22:48:10 -0700

    Fixed import for user_router

--- scan-and-save/backend/app/api/__init__.py
+++ scan-and-save/backend/app/api/__init__.py
@@ -1,5 +1,5 @@
 from .routes.health import router as health_router
 from .routes.auth import router as authenticator_router
 from .routes.admin import router as admin_router
-from .routes.users import router as users_router
+from .routes.users import router as user_router
 from .routes.home import router as home_router
