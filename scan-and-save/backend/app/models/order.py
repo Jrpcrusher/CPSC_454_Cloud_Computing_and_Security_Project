68abb4e791d19bd6ff884176bee6a1d43611cf64
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-03-13 22:45:51 -0700

    Implemented models for retrieving data

---
+++ scan-and-save/backend/app/models/order.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+from .user import UserSummary
+from uuid import UUID
+from enum import Enum
+
+class Status(str, Enum):
+    received = "received"
+    in_progress = "in_progress"
+    completed = "completed"
+    closed = "closed"
+    cancelled = "cancelled"
+    awaiting_payment = "awaiting_payment"
+
+class Order(BaseModel): # Generic order model
+    order_id: UUID
+    client: UserSummary
+    artist: UserSummary
+    order_details: str
+    creation_date: datetime
+    status: Status
--- scan-and-save/backend/app/models/user.py
+++ scan-and-save/backend/app/models/user.py
@@ -1,18 +1,60 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional, Literal
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
+from uuid import UUID
+from enum import Enum
 
-class CreateUser(BaseModel): # The data model for creating a new user, these are the values needed
+class UserRole(str, Enum):
+    user = "user"
+    admin = "admin"
+
+class CreateUser(BaseModel): # For user creation
+    username: str = Field(min_length=3, max_length=30, pattern="^[a-zA-Z0-9_]+$")
     email: EmailStr
-    username: str
-    password: str
+    password: str = Field(min_length=12, max_length=128)
 
-class UserResponse(BaseModel):
-    id: str
+class PublicProfile(BaseModel): # Return a users public profile
+    user_id: UUID
+    username: str = Field(min_length=3, max_length=30)
+    register_date: datetime
+    role: UserRole
+    pfp_path: str | None = None
+    description: str | None = Field(max_length=500)
+
+class ViewMe(BaseModel): # For viewing own profile
+    user_id: UUID
+    username: str = Field(min_length=3, max_length=30)
     email: EmailStr
-    username: str
-    role: str
-    createdAt: datetime
+    register_date: datetime
+    role: UserRole
+    pfp_path: str | None = None
+    description: str | None = Field(max_length=500)
 
-class UpdateRole(BaseModel):
-    role: Literal["user", "admin"]
+class Settings(BaseModel): # For viewing settings
+    username: str = Field(min_length=3, max_length=30)
+    email: EmailStr
+    pfp_path: str | None
+    description: str | None = Field(max_length=500)
+
+class UserProfileAdminView(BaseModel): # For viewing profile as admin
+    user_id: UUID
+    username: str = Field(min_length=3, max_length=30)
+    email: EmailStr
+    register_date: datetime
+    role: UserRole
+    description: str | None = Field(max_length=500)
+    pfp_path: str | None = None
+
+class UserPermissions(BaseModel): # For viewing permissions of user
+    username: str = Field(min_length=3, max_length=30)
+    role: UserRole
+    pfp_path: str | None = None
+
+class LoginRequest(BaseModel): # For handling user login requests
+    email: EmailStr
+    password: str = Field(min_length=12, max_length=128)
+
+class UserSummary(BaseModel):
+    email: EmailStr
+    user_id: UUID
+    username: str = Field(min_length=3, max_length=30)
+    pfp_path: str | None = None
