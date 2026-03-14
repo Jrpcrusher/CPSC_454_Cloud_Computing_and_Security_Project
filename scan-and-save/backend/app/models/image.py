51dd70626f6c31fa153e321745da0b2d825219d7
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-03-13 22:46:23 -0700

    Added model for image.py (Forgot to add last commit)

---
+++ scan-and-save/backend/app/models/image.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from .user import UserSummary
+from uuid import UUID
+
+class Image(BaseModel): # Generic image model
+    image_id: UUID
+    image_path: str
+    artist: UserSummary
+    upload_date: datetime
+    description: str | None = Field(max_length=500)
