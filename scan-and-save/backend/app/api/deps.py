0863b14da1863a0a448599c5e0d67d40e82351cc
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-03-19 23:26:37 -0700

    Storing changes

--- scan-and-save/backend/app/api/deps.py
+++ scan-and-save/backend/app/api/deps.py
@@ -1,4 +1,40 @@
-from fastapi import Request
+from fastapi import Request, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from ..services import db_service
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from ..core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_db(request: Request):
     return request.app.database
+
+def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+    payload = decode_token(token)
+    user_id = payload.get("user_id")
+
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid Token")
+    
+    user = db_service.get_user(user_id, db)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+    
+    return user
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_token(token: str):
+    try:
+        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
--- scan-and-save/backend/app/core/config.py
+++ scan-and-save/backend/app/core/config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     DB_NAME: str
     STRIPE_KEY: str | None = None # We either have the stripe key or not
     AWS_REGION: str | None = None # We have the AWS region, or not
+    SECRET_KEY = str # the key to hash with
+    ALGORITHM = "HS256" # Signing Algorithm we chose, HMAC + SHA-256
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60 # How long our token is valid for
 
     class Config:
         env_file = ".env"
