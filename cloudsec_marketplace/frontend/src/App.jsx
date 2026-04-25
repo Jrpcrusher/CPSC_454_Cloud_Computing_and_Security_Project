92bd603ac6bffffe90f1b57c5f17685cb5f99d9b
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-04-25 14:47:30 -0700

    Initialized routes for admins in App.jsx

--- cloudsec_marketplace/frontend/src/App.jsx
+++ cloudsec_marketplace/frontend/src/App.jsx
@@ -18,6 +18,15 @@ import ImageDetail from "./pages/ImageDetail";
 import PublicImageDetail from "./pages/PublicImageDetail";
 import HealthStatus from "./pages/HealthStatus";
 
+/* Admin pages */
+import AdminUsers from "./pages/AdminUsers";
+import AdminUserDetail from "./pages/AdminUserDetail";
+import AdminUserImages from "./pages/AdminUserImages";
+import AdminImageDetail from "./pages/AdminImageDetail";
+import AdminUserOrders from "./pages/AdminUserOrders";
+import AdminOrderDetail from "./pages/AdminOrderDetail";
+import AdminPermissions from "./pages/AdminPermissions";
+
 import "./App.css";
 
 function App() {
@@ -26,22 +35,38 @@ function App() {
       <div className="app">
         <Navbar />
         <Routes>
+          {/* Public */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/status/health" element={<HealthStatus />} />
+
+          {/* Public creator browsing */}
           <Route path="/creator/:userId" element={<CreatorProfile />} />
           <Route path="/creator/:userId/request" element={<ArtRequest />} />
+          <Route path="/creator/:userId/images/:imageId" element={<PublicImageDetail />} />
+
+          {/* User area */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/become-creator" element={<BecomeCreator />} />
           <Route path="/profile/edit" element={<EditProfile />} />
           <Route path="/orders/:orderId" element={<OrderDetail />} />
+          <Route path="/dashboard/portfolio" element={<PortfolioManager />} />
+          <Route path="/dashboard/portfolio/:imageId" element={<ImageDetail />} />
+          <Route path="/dashboard/transactions" element={<Transactions />} />
+
+          {/* Stripe onboarding */}
           <Route path="/artist/onboard/complete" element={<OnboardComplete />} />
           <Route path="/artist/onboard/refresh" element={<OnboardRefresh />} />
-          <Route path="/dashboard/portfolio" element={<PortfolioManager />} />
-          <Route path="/dashboard/transactions" element={<Transactions />} />
-          <Route path="/dashboard/portfolio/:imageId" element={<ImageDetail />} />
-          <Route path="/creator/:userId/images/:imageId" element={<PublicImageDetail />} />
-          <Route path="/status/health" element={<HealthStatus />} />
+
+          {/* Admin */}
+          <Route path="/admin/users" element={<AdminUsers />} />
+          <Route path="/admin/users/:userId" element={<AdminUserDetail />} />
+          <Route path="/admin/users/:userId/permissions" element={<AdminPermissions />} />
+          <Route path="/admin/users/:userId/images" element={<AdminUserImages />} />
+          <Route path="/admin/users/:userId/images/:imageId" element={<AdminImageDetail />} />
+          <Route path="/admin/users/:userId/orders" element={<AdminUserOrders />} />
+          <Route path="/admin/users/:userId/orders/:orderId" element={<AdminOrderDetail />} />
         </Routes>
       </div>
     </AuthProvider>
